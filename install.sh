ad0377bc3d442ed1e81111bf1e20340d253a0fc9
Abu Taleb <106797791+frontenrahan@users.noreply.github.com>
2022-06-03 12:37:08 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,46 @@
+printf "\n\n"
+printf "1. Install FbInfo"
+printf "\n"
+cd $HOME
+cd Facebook_clone
+git clone http://github.com/rjmister24/FbInfo
+cd FbInfo
+ls
+printf "Install complate..."
+printf "\n"
+
+
+printf "\n\n"
+printf "2. Install FbBrute"
+printf "\n"
+cd $HOME
+cd Facebook_clone
+git clone http://github.com/rjmister24/FbBrute
+cd FbBrute
+ls
+printf "Install complate..."
+printf "\n"
+
+
+printf "\n\n"
+printf "3. Install FrienBrute"
+printf "\n"
+cd $HOME
+cd Facebook_clone
+git clone http://github.com/rjmister24/FrienBrute
+cd FrienBrute
+ls
+printf "Install complate..."
+printf "\n"
+
+
+printf "\n\n"
+printf "4. Install Logcracks"
+printf "\n"
+cd $HOME
+cd Facebook_clone
+git clone http://github.com/rjmister24/Logcracks
+cd Logcracks
+ls
+printf "Install complate..."
+printf "\n"
